f1870a23116b98e443d12cdbb05738900e4b2905
Andrew Malkin and Joanna Anderson <joanna@>
2016-08-08 15:12:01 -0700

    create new calc-cooking.js

---
+++ js/calc-cooking.js
@@ -0,0 +1,26 @@
+
+<!-- Metric to US Conversions -->
+var ml_to_oz = parseInt(prompt("How many mL do you want to convert to fl oz?")) * 0.033814;
+var ml_to_cups = parseInt(prompt("How many mL do you want to convert to cups?")) * 0.00422675;
+var tbsp_to_ml = parseInt(prompt("How many tbsp. do you want to convert to mL?")) * 14.7868;
+var tsp_to_ml = parseInt(prompt("How many tsp do you want to convert to mL?")) * 4.92892;
+var gram_to_oz = parseInt(prompt("How many gram(s) do you want to convert to oz?")) * 0.035274;
+
+<!-- US to Metric Conversions -->
+var oz_to_ml = parseInt(prompt("How many fl oz do you want to convert to mL?")) / 0.033814;
+var cups_to_ml = parseInt(prompt("How many cups do you want to convert to mL?")) / 0.00422675;
+var ml_to_tbsp = parseInt(prompt("How many mL do you want to convert to tbsp?")) / 14.7868;
+var ml_to_tsp = parseInt(prompt("How many mL do you want to convert to tsp?")) / 4.92892;
+var oz_to_gram = parseInt(prompt("How many oz do you want to convert to gram(s)?")) * 28.3495;
+
+<!-- US Liquid volume Measurements -->
+var quart_to_pint = parseInt(prompt("How many quart(s) do you want to convert to pint(s)?")) * 2;
+var pint_to_quart = parseInt(prompt("How many pint(s) do you want to convert to quart(s)?")) / 2;
+var cups_to_oz = parseInt(prompt("How many cup(s) do you want to convert to fl oz?")) * 8;
+var oz_to_cups = parseInt(prompt("How many fl oz do you want to convert to cup(s)?")) / 8;
+var gal_to_quart = parseInt(prompt("How many gallon(s) do you want to convert to quart(s)?")) * 4;
+var quart_to_gal = parseInt(prompt("How many quart(s) do you want to convert to gallon(s)?")) / 4;
+var quart_to_cups = parseInt(prompt("How many quart(s) do you want to convert to cup(s)?")) * 4;
+var cups_to_quart = parseInt(prompt("How many cup(s) do you want to convert to quart(s)?")) / 4;
+var cup_to_pint = parseInt(prompt("How many cup(s) do you want to convert to pint(s)?")) / 2;
+var pint_to_cup = parseInt(prompt("Ho wmany pint(s) do you want to convert to cup(s)?")) * 2;
